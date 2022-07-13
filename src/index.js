c786bc86512e0ec7c14d3a24437ecd4cdab86a10
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-13 09:46:05 +0200

    Update branch.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import _ from 'lodash';
+import './index.css';
+import addElem from './modules/addElement';
+import TaskList from './modules/classTaskList';
+import refreshList from './modules/refresh';
 
-function component() {
-  const element = document.createElement('div');
+const taskList = new TaskList();
+// dom
+const mainContainer = document.querySelector('.todo-list-container');
+// HTML skeleton
+// Header (Title and input)
+mainContainer.innerHTML = `<div class="row">
+<h1>Today's To Do</h1>
+<i class="fa-solid fa-rotate fa-lg font-awesome-icon"></i>
+</div>`;
+const inputContainer = addElem('form', [], mainContainer);
+const inputText = addElem('input', ['input-add-task'], inputContainer);
+inputText.setAttribute('placeholder', 'Add to your list...');
+addElem('i', ['fa-solid', 'fa-arrow-right-to-bracket', 'fa-sm', 'font-awesome-icon'], inputContainer);
+// Main (list)
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const listContainer = addElem('div', [], mainContainer);
+// Bottom (button)
+const clearBtn = addElem('button', ['button'], mainContainer);
+clearBtn.textContent = 'Clear all completed';
 
-  return element;
-}
+// Input
+inputContainer.onsubmit = (e) => {
+  e.preventDefault();
+  taskList.addTask(inputText.value);
 
-document.body.appendChild(component());
+  inputContainer.reset();
+  refreshList(taskList, listContainer);
+};
+
+// clear button
+clearBtn.onclick = () => {
+  const data = JSON.parse(localStorage.getItem('taskList'));
+
+  const newData = data.filter((task) => !task.completed === true);
+  localStorage.setItem('taskList', JSON.stringify(newData));
+  window.location.reload();
+};
+
+// On load
+refreshList(taskList, listContainer);
